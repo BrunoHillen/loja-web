7fecbff31f96df7e282b902fa880134caa0ef513
Alberto <betim009@gmail.com>
2024-09-11 12:55:04 -0300

    update 2

--- backend/connection.js
+++ backend/connection.js
@@ -3,7 +3,7 @@ const mysql = require('mysql2/promise');
 const connection = mysql.createPool({
     host: 'localhost', // Seu host do MySQL
     user: 'root', // Seu usuário do MySQL
-    password: '1234', // Sua senha do MySQL // 120522
+    password: '120522', // Sua senha do MySQL // 120522
     database: 'loja', // nome do BD 
     waitForConnections: true,
     connectionLimit: 10,
